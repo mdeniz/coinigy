deb452ae289f08866bd8d7ff1bc1234179f6e4af
mdeniz <mdeniz@suse.com>
2018-02-13 15:23:33 +0100

    Finish the MarketData module

--- lib/coinigy/client/market_data.rb
+++ lib/coinigy/client/market_data.rb
@@ -1,7 +1,27 @@
 module Coinigy
   class Client
+    # Access exchange and market data
     module MarketData
+      # Returns a list of all supported exchanges
+      def exchanges
+        request('exchanges')
+      end
 
+      # Returns a list of markets on specified exchange
+      def markets(id = nil)
+        request('markets', { exchange_code: id })
+      end
+
+      # Trade history, asks and bids for any supported exchange/market
+      # type: Can be 'all', 'history', 'asks', 'bids' or 'orders'
+      def data(options = {})
+        request('data', options.slice('exchange_code', 'exchange_market', 'type'))
+      end
+
+      # Returns last, high (24h), low (24h), ask, bid for specified market
+      def ticker(options = {})
+        request('ticker', options.slice('exchange_code', 'exchange_market'))
+      end
     end
   end
 end
