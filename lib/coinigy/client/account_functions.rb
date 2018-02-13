28a0e5a426f50418460fa00c033b6163d615f33b
mdeniz <mdeniz@suse.com>
2018-02-13 15:14:29 +0100

    Finished all the methods of AccountFunctions module

--- lib/coinigy/client/account_functions.rb
+++ lib/coinigy/client/account_functions.rb
@@ -36,6 +36,37 @@ module Coinigy
       def delete_alert(id = nil)
         request('deleteAlert', { alert_id: id })
       end
+
+      # Add a new Exchange API Key to your account. Returns newly-generated auth_id in 'data' block.
+      # After adding a new key, it must still be both activated and activated for trading.
+      def add_api_key(options = {})
+        request('addApiKey', options.slice('api_key', 'api_secret', 'api_exch_id', 'api_nickname'))
+      end
+
+      # Delete specified Exchange API Account by auth_id
+      def delete_api_key(id = nil)
+        request('deleteApiKey', { auth_id: id })
+      end
+
+      # Activate/Enable API Key usage (enables automatic balance monitoring)
+      def activate_api_key(id = nil, auth_active = 1)
+        request('activateApiKey', { auth_id: id, auth_active: auth_active })
+      end
+
+      # Activate/Enable Trading on specified API key
+      def activate_trading_key(id = nil, auth_active = 1)
+        request('activateTradingKey', { auth_id: id, auth_active: auth_active })
+      end
+
+      # Create a new exchange order. Returns internal_order_id upon success
+      def add_order(options = {})
+        request('addOrder', options.slice('auth_id', 'exch_id', 'mkt_id', 'order_type_id', 'price_type_id', 'limit_price', 'order_quantity'))
+      end
+
+      # Cancel an outstanding exchange order
+      def cancel_order(id = nil)
+        request('cancelOrder', { internal_order_id: id })
+      end
     end
   end
 end
