0cba4e298f8a28077434d232dd9fac128bcd9fe1
mdeniz <mdeniz@suse.com>
2018-02-15 17:56:52 +0100

    Add data and ticker info methods for Market class

--- lib/coinigy/market.rb
+++ lib/coinigy/market.rb
@@ -14,5 +14,55 @@ module Coinigy
         'mkt_name' => mkt_name,
         'exchmkt_id' => exchmkt_id }
     end
+
+    def history
+      get_data(:history).data['history']
+    end
+
+    def asks
+      get_data(:asks).data['asks']
+    end
+
+    def bids
+      get_data(:bids).data['bids']
+    end
+
+    def orders
+      orders_data = get_data(:orders)
+      {
+        asks: orders_data['asks'],
+        bids: orders_data['bids']
+      }
+    end
+
+    def data
+      all_data = get_data(:all)
+      {
+        history: all_data['history'],
+        asks: all_data['asks'],
+        bids: all_data['bids']
+      }
+    end
+
+    def ticker
+      values = exchange.subscription.client.ticker('exchange_code' => exch_code,
+                                                   'exchange_market' => mkt_name).data.first
+      {
+        last_trade: values['last_trade'],
+        high_trade: values['high_trade'],
+        low_trade: values['low_trade'],
+        volume: values['current_volume'],
+        timestamp: values['timestamp'],
+        ask: values['ask'],
+        bid: values['bid']
+      }
+    end
+    private
+
+    def get_data(type = :all)
+      exchange.subscription.client.data('exchange_code' => exch_code,
+                                        'exchange_market' => mkt_name,
+                                        'type' => type.to_s)
+    end
   end
 end
